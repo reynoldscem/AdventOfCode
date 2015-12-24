83d50033b5f2c87fe6f9021cc0ee94b299ff7cc5
Charles Reynolds <reynoldscem@gmail.com>
2015-12-24 19:50:20 +0000

    Day 14

---
+++ 14/1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+require 'pry'
+
+def timeForReindeer(reindeer, totalTime)
+  completeCycles = totalTime / (reindeer.duration + reindeer.rest)
+  remainingTime = [totalTime % (reindeer.duration + reindeer.rest), reindeer.duration].min
+  completeCycles * reindeer.speed * reindeer.duration + remainingTime * reindeer.speed
+end
+
+begin
+  input = File.open(ARGV[0]).read.split("\n").map(&:split)
+  duration = ARGV[1].to_i
+rescue
+  puts "Oops"
+else
+  Reindeer = Struct.new(:name, :speed, :duration, :rest)
+  reindeers = input.map do |line|
+    Reindeer.new(line[0], line[3].to_i, line[6].to_i, line[-2].to_i)
+  end
+  reindeerTimes = reindeers.map do |reindeer|
+    timeForReindeer(reindeer, duration)
+  end
+  puts reindeerTimes.max
+end
---
+++ 14/2.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+require 'pry'
+
+def timeForReindeer(reindeer, totalTime)
+  completeCycles = totalTime / (reindeer.duration + reindeer.rest)
+  remainingTime = [totalTime % (reindeer.duration + reindeer.rest), reindeer.duration].min
+  completeCycles * reindeer.speed * reindeer.duration + remainingTime * reindeer.speed
+end
+
+begin
+  input = File.open(ARGV[0]).read.split("\n").map(&:split)
+  duration = ARGV[1].to_i
+rescue
+  puts "Oops"
+else
+  Reindeer = Struct.new(:name, :speed, :duration, :rest, :points)
+  reindeers = input.map do |line|
+    Reindeer.new(line[0], line[3].to_i, line[6].to_i, line[-2].to_i, 0)
+  end
+  (1..duration).each do |duration|
+    reindeerTimes = reindeers.map do |reindeer|
+      timeForReindeer(reindeer, duration)
+    end
+    sorted = Hash[reindeers.zip(reindeerTimes)].sort_by{|_,k|k}
+    bests = sorted.select{|reindeer| reindeer[1] == sorted.last[1]}
+    bests.each do |reindeer|
+      reindeer[0].points += 1
+    end
+  end
+  puts reindeers.sort_by {|reindeer| reindeer.points}.last.points
+end
