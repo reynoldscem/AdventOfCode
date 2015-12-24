ceba80a56d8dd4594ceec6c49fed73f09dad24e9
Charles Reynolds <reynoldscem@gmail.com>
2015-12-24 20:52:09 +0000

    17

---
+++ 17/1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+begin
+  input = File.open(ARGV[0]).read.split("\n").map(&:to_i)
+rescue
+  puts "Oops"
+else
+  combs = (1..(input.length)).flat_map{|n| input.combination(n).to_a}
+  combLengths = combs.map{|arr| arr.reduce(:+)}
+  puts combLengths.select{|capacity| capacity == 150}.length
+end
---
+++ 17/2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+begin
+  input = File.open(ARGV[0]).read.split("\n").map(&:to_i)
+rescue
+  puts "Oops"
+else
+  combs = (1..(input.length)).flat_map{|n| input.combination(n).to_a}
+  combLengths = combs.map{|arr| arr.reduce(:+)}
+  numElems = combs.map{|entry| entry.length}
+  zipped = numElems.zip(combLengths)
+  successful = zipped.select{|entry| entry[1] == 150}
+  minNum = successful.first[0]
+  puts successful.select{|entry| entry[0] == minNum}.length
+end
