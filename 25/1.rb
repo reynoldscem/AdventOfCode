685d52ede13d07150350a9eb3b7e172628a44526
Charles Reynolds <reynoldscem@gmail.com>
2015-12-26 16:08:46 +0000

    25

---
+++ 25/1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+begin
+  input = File.open(ARGV[0]).read
+rescue
+  puts "Oops"
+else
+  rows = input.split[-3].to_i
+  cols = input.split[-1].to_i
+  n = rows + cols;
+  iter = (n - 2) * (n - 1) / 2 + cols - 1
+  val = 20151125
+  multiplier = 252533
+  moduloBy = 33554393
+  iter.times do
+    val = (val * multiplier) % moduloBy
+  end
+  puts val
+end
