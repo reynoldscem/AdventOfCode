c0d6c8ac21c7b5f2ca0cabc208a0171372821415
Charles Reynolds <reynoldscem@gmail.com>
2016-01-03 17:21:55 +0000

    More error handling

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,18 +5,23 @@
 #include <numeric>
 
 int main(int argc, char** argv) {
-  std::ifstream infile;
-  infile.open(argv[1]);
-  if (infile.is_open()) {
-    std::string brackets;
-    getline(infile, brackets);
-    infile.close();
-    std::cout <<
-      std::accumulate(brackets.begin(), brackets.end(), 0,
-        [](int acc, char c) -> int { return acc + (c == '(' ? 1 : -1); }
-      );
-    std::cout << '\n';
-    return 0;
+  if (argv[1] != NULL) {
+    std::ifstream infile;
+    infile.open(argv[1]);
+    if (infile.is_open()) {
+      std::string brackets;
+      getline(infile, brackets);
+      infile.close();
+      std::cout <<
+        std::accumulate(brackets.begin(), brackets.end(), 0,
+          [](int acc, char c) -> int { return acc + (c == '(' ? 1 : -1); }
+        );
+      std::cout << '\n';
+      return 0;
+    } else {
+      std::cout << "File could not be opening for reading.\n";
+      return -1;
+    }
   } else {
     std::cout << "Valid input file from AdventOfCode required as first argument.\n";
     return -1;
--- 1/2.cpp
+++ 1/2.cpp
@@ -5,22 +5,27 @@
 #include <numeric>
 
 int main(int argc, char** argv) {
-  std::ifstream infile;
-  infile.open(argv[1]);
-  if (infile.is_open()) {
-    std::string brackets;
-    getline(infile, brackets);
-    infile.close();
-    int numToks = brackets.length();
-    int nums[numToks];
-    std::transform(brackets.begin(), brackets.end(), nums,
-      [](char c) -> int { return(c == '(' ? 1 : -1); }
-    );
-    int pos = 0;
-    for (int floor = 0; floor != -1; pos++)
-      floor += nums[pos];
-    std::cout << pos << "\n";
-    return 0;
+  if (argv[1] != NULL) {
+    std::ifstream infile;
+    infile.open(argv[1]);
+    if (infile.is_open()) {
+      std::string brackets;
+      getline(infile, brackets);
+      infile.close();
+      int numToks = brackets.length();
+      int nums[numToks];
+      std::transform(brackets.begin(), brackets.end(), nums,
+        [](char c) -> int { return(c == '(' ? 1 : -1); }
+      );
+      int pos = 0;
+      for (int floor = 0; floor != -1; pos++)
+        floor += nums[pos];
+      std::cout << pos << "\n";
+      return 0;
+    } else {
+      std::cout << "File could not be opened for reading.\n";
+      return -1;
+    }
   } else {
     std::cout << "Valid input file from AdventOfCode required as first argument.\n";
     return -1;
