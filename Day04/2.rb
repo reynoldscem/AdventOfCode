00dc55811f1cbcc5a9c3e75c52d79b8c54e342ba
Charles EM Reynolds <reynoldscem@gmail.com>
2022-12-12 14:02:15 +0000

    Separate 1 and 2

--- Day04/2.rb
+++ Day04/2.rb
@@ -5,7 +5,7 @@ input = File.open(ARGV[0]).read.chomp
 
 (1..Float::INFINITY).each do |num|
   hash = Digest::MD5.hexdigest((input + num.to_s))
-  if hash[0...5] == '00000'
+  if hash[0...6] == '000000'
     puts num
     break
   end
